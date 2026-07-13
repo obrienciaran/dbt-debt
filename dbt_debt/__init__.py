04026246645cf55210d78b12909eac2187b32da5
Ciaran O'Brien <ciaranobrien@Ciarans-MacBook-Pro.local>
2026-07-13 19:15:41 +0200

    :memo: Package docstring names all three warehouses

--- dbt_debt/__init__.py
+++ dbt_debt/__init__.py
@@ -1,3 +1,3 @@
-"""dbt-debt: a technical-debt scorecard for dbt projects on BigQuery."""
+"""dbt-debt: a technical-debt scorecard for dbt projects on BigQuery, Snowflake, and Redshift."""
 
 __version__ = "0.0.1"
